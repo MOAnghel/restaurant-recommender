1d7bc1f6f73ba56bf0b70ed85cb00e6e32464260
Mircea Anghel <mirceaanghel01@gmail.com>
2026-05-12 10:50:32 +0300

    Initial commit: Restaurant recommendation

---
+++ backend/restaurant-recommender/src/test/java/com/restaurant/recommender/RestaurantRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.recommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
